68deb62c2a55b5b21fd4add2ac9e7cdd56424f85
GeorgiNzm <georginzm@gmail.com>
2018-05-02 20:43:06 +0300

    Removing some controllers

--- src/main/java/org/elsys/ip/online_gun_shop/controller/PurchaseController.java
+++
@@ -1,9 +0,0 @@
-package org.elsys.ip.online_gun_shop.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/purchases")
-public class PurchaseController {
-}
